59fc4bf19558a225e88889198478093707575db2
johnhadaway <johnhadaway8@gmail.com>
2023-02-18 14:20:46 +0200

    added part1 exercises 1.6-1.11 (unicafe)

---
+++ part1/unicafe/src/App.js
@@ -0,0 +1,73 @@
+import { useState } from 'react';
+
+const Button = ({ onClick, text }) => (
+  <button onClick={onClick}>{text}</button>
+);
+
+const StatisticsCell = ({ text, value }) => (
+  <tr>  
+    <td>{text}</td>
+    <td>{value}</td>
+  </tr>
+);
+
+const Statistics = ({ good, neutral, bad }) => {
+  const calculateTotal = () => {
+    return good + neutral + bad;
+  };
+
+  const calculateAverageScore = () => {
+    return (good - bad) / (good + neutral + bad);
+  };
+
+  const calculatePercentagePositive = () => {
+    return (good / (good + neutral + bad)) * 100;
+  };
+
+  return (
+    <div> 
+      <h1>statistics</h1>
+      <table>
+        <tbody>
+          <StatisticsCell text="Good" value={good} />
+          <StatisticsCell text="Neutral" value={neutral} />
+          <StatisticsCell text="Bad" value={bad} />
+          <StatisticsCell text="Total" value={calculateTotal()} />
+          <StatisticsCell text="Average Score" value={calculateAverageScore()} />
+          <StatisticsCell text="Percentage Positive" value={calculatePercentagePositive()} />
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+const App = () => {
+  const [feedback, setFeedback] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
+
+  const [hasFeedback, setHasFeedback] = useState(false);
+
+  const handleFeedback = (feedback) => () => {
+    setFeedback((prev) => ({
+      ...prev,
+      [feedback]: prev[feedback] + 1,
+    }));
+    setHasFeedback(true);
+  };
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+      <Button onClick={handleFeedback('good')} text="Good" />
+      <Button onClick={handleFeedback('neutral')} text="Neutral" />
+      <Button onClick={handleFeedback('bad')} text="Bad" />
+      {hasFeedback ? 
+        (<Statistics good={feedback.good} neutral={feedback.neutral} bad={feedback.bad}/>) : null}
+    </div>
+  )
+};
+
+export default App;
