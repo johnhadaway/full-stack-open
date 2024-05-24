111271f1fc13b008c415d822ab52d120b7539073
John Hadaway <johnhadaway8@gmail.com>
2024-05-24 14:45:42 +0300

    exercises 3.13-3.14

--- part3/index.js
+++ part3/index.js
@@ -11,28 +11,7 @@ app.use(express.static('dist'))
 app.use(cors())
 morgan.token('body', (request) => JSON.stringify(request.body))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+let persons = []
 
 app.get('/api/persons', (request, response) => {
     Person.find({}).then(persons => {
@@ -40,6 +19,7 @@ app.get('/api/persons', (request, response) => {
     })
 })
 
+// Needs to be updated to use MongoDB
 app.get('/info', (request, response) => {
     const date = new Date()
     response.send(`<p>Phonebook has info for ${persons.length} people</p><p>${date}</p>`)
@@ -52,9 +32,12 @@ app.get('/api/persons/:id', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(person => person.id !== id)
-    response.status(204).json()
+    Person.findByIdAndDelete(request.params.id)
+        .then(result => {
+            console.log(result)
+            response.status(204).end()
+        })
+        .catch(error => next(error))      
 })
 
 const postLogger = morgan(':method :url :status :res[content-length] - :response-time ms :body')
